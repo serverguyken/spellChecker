272f3d3554686a999b015429ea7aeaefdcaa3700
Serverguyken <serverguyken@gmail.com>
2021-09-30 10:27:36 -0400

    first commit

---
+++ src/SpellChecker.ts
@@ -0,0 +1,64 @@
+import { words, isWord, getWord, getExample, getDefinition } from "./words"
+
+const levenshteinDistance = (str1: string, str2: string) => {
+    const track = Array(str2.length + 1).fill(null).map(() =>
+        Array(str1.length + 1).fill(null));
+    for (let i = 0; i <= str1.length; i += 1) {
+        track[0][i] = i;
+    }
+    for (let j = 0; j <= str2.length; j += 1) {
+        track[j][0] = j;
+    }
+    for (let j = 1; j <= str2.length; j += 1) {
+        for (let i = 1; i <= str1.length; i += 1) {
+            const indicator = str1[i - 1] === str2[j - 1] ? 0 : 1;
+            track[j][i] = Math.min(
+                track[j][i - 1] + 1, // deletion
+                track[j - 1][i] + 1, // insertion
+                track[j - 1][i - 1] + indicator, // substitution
+            );
+        }
+    }
+    return track[str2.length][str1.length];
+};
+const findClosestWords = (w: string): string => {
+    if (w === "") {
+        return "Please provide a word for suggestion"
+    }
+    let result: any= "";
+    let closestWords = words.sort((a: any, b: any) => levenshteinDistance(w, a.word) - levenshteinDistance(w, b.word));
+    let closestWordsPercent = closestWords.filter((word: any) => levenshteinDistance(w, word.word) <= 1);
+    let closestWord = closestWords.find((word: any) => levenshteinDistance(w, word.word) === 0);
+    if (closestWord) {
+        result = closestWord.word;
+    }
+    else if (closestWordsPercent.length > 0) {
+        result = `Did you mean: ${closestWordsPercent[0].word} instead of ${w}?`;
+    }
+    else {
+        result = `No suggestions found for ${w}`;
+    }
+    return result;
+}
+
+
+
+const checkWord = (w: string) => {
+    if (w === "") {
+        return "Please provide a word for suggestion"
+    }
+    let suggestion: string = "";
+    let word = w.toLowerCase()
+    let wSplit = word.split("");
+    for (word in wSplit) {
+        if(isWord(w)) {
+            suggestion = "Correct spelling"
+        }
+        else {
+            suggestion = findClosestWords(w)
+        }
+    }
+    return suggestion
+}
+
+console.log(checkWord("script"));
---
+++ src/words.ts
@@ -0,0 +1,126 @@
+interface Dictionary {
+    word: string;
+    definition: string;
+    example: string;
+}
+
+const words: Array<Dictionary> = [
+    {
+        word: 'hello',
+        definition: 'greeting',
+        example: 'hello world'
+    },
+    {
+        word: 'world',
+        definition: 'planet',
+        example: 'hello world'
+    },
+    {
+        word: 'javascript',
+        definition: 'programming language',
+        example: 'hello javascript'
+    },
+    {
+        word: 'typescript',
+        definition: 'programming language',
+        example: 'hello typescript'
+    },
+    {
+        word: 'node',
+        definition: 'programming language',
+        example: 'hello node'
+    },
+    {
+        word: "happy",
+        definition: "feeling of contentment or satisfaction",
+        example: "I am happy to see you"
+    },
+    {
+        word: "sad",
+        definition: "feeling of unhappiness or sorrow",
+        example: "I am sad to see you"
+    },
+    {
+        word: "angry",
+        definition: "feeling of strong feeling of anger",
+        example: "I am angry to see you"
+    },
+    {
+        word: "bad",
+        definition: "of poor quality or a low standard",
+        example: "a bad diet"
+    }
+]
+
+
+function isWord(word: string): boolean {
+    if (words.find(w => w.word === word.toLowerCase())) {
+        return true
+    } else {
+        return false
+    }
+}
+
+function addWord(word: string, definition: string, example: string): string {
+    if (words.find(w => w.word === word)) {
+        return `${word} is already added to the dictionary`
+    } else {
+        words.push({
+            word,
+            definition,
+            example
+        })
+        return `${word} has been added to the dictionary`
+    }
+}
+
+function removeWord(word: string): string {
+    if (words.find(w => w.word === word)) {
+        words.splice(words.indexOf(words.find(w => w.word === word)), 1)
+        return `${word} has been removed from the dictionary`
+    } else {
+        return `${word} does not exist in the dictionary`
+    }
+}
+
+function getWord(word: string): any {
+    if (words.find(w => w.word === word)) {
+        return words.find(w => w.word === word.toLowerCase())
+    } else {
+        return `${word} does not exist in the dictionary`
+    }
+}
+
+function getAllWords(): {} {
+    return words
+}
+
+function getExample(word: string): string {
+    if (words.find(w => w.word === word)) {
+        return `Example for ${word}: ${words.find(w => w.word === word).example}`
+    } else {
+        return `Cannot find an example for ${word}, ${word} does not exist in the dictionary`
+    }
+}
+
+function getAllExamples(): any {
+    return words.map(w => {
+        return `Word: ${w.word}, Example: ${w.example}`
+    })
+}
+
+function getDefinition(word: string): string {
+    if (words.find(w => w.word === word)) {
+        return `Definition for ${word}: ${words.find(w => w.word === word).definition}`
+    } else {
+        return `Cannot find a definition for ${word}, ${word} does not exist in the dictionary`
+    }
+}
+
+function getAllDefinitions(): any {
+    return words.map(w => {
+        return `Word: ${w.word}, Definition: ${w.definition}`
+    })
+}
+
+export { words, isWord,  addWord, removeWord, getWord, getAllWords, getExample, getAllExamples, getDefinition, getAllDefinitions};
